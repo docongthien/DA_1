221558b45655745478e770db6dfc8886e50a4b19
Your Name <thiendcph46905@fpt.edu.vn>
2024-07-12 11:09:46 +0700

    them class 1

---
+++ Du_An_1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Du_An_1
+{
+    internal class Class1
+    {
+        //code
+    }
+}
