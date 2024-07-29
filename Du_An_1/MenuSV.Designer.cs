12ef74554c5f625edd4af27fb6f85ac457952007
Your Name <thiendcph46905@fpt.edu.vn>
2024-07-29 10:06:34 +0700

    29/7

---
+++ Du_An_1/MenuSV.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Du_An_1
+{
+    partial class MenuSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(650, 322);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 7;
+            button4.Text = "Thoát";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(490, 99);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 88);
+            button3.TabIndex = 6;
+            button3.Text = "Hỗ Trợ";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(276, 99);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 88);
+            button2.TabIndex = 5;
+            button2.Text = "Điểm Sinh Viên";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(56, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(170, 88);
+            button1.TabIndex = 4;
+            button1.Text = "Thông Tin Sinh viên";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // MenuSV
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Turquoise;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MenuSV";
+            Text = "MenuSV";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+    }
+}
